c65d28e99eb3f89f468be8a16cbbeffcb4395857
polina <greenowl403@gmail.com>
2021-01-12 21:32:12 +0300

    add new elements on main page

--- 44brew/main.js
+++ 44brew/main.js
@@ -1,28 +1,39 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1;
 
-// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function showMap() {
+    let dialog = document.querySelector('.map-dialog');
+    dialog.showModal();
+}
+
+function closeMap() {
+    let dialog = document.querySelector('.map-dialog');
+    dialog.close();
 }
